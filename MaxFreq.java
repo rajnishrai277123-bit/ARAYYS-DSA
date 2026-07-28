2413545b4e605c683e41eb9db35441757c081050
Rajnish rai <rajnishrai277123@gmail.com>
2026-07-28 19:59:36 +0530

    commit

---
+++ MaxFreq.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class MaxFreq {
+
+    static int frequency(int[] arr) {
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        int maxfreq = -1;
+        int maxfreqkey = -1;
+
+        for (int key : freq.keySet()) {
+            int currentKey = key;
+            int currentKeyFreq = freq.get(key);
+
+            if (currentKeyFreq > maxfreq) {
+                maxfreq = currentKeyFreq;
+                maxfreqkey = currentKey;
+            }
+        }
+
+        return maxfreqkey;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 8, 4, 9};
+
+        System.out.println(frequency(arr));
+    }
+}
